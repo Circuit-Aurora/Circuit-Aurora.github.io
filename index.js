517634fa4d99ae873a180ee5dd4059cf6ac8de45
Circuit-Aurora <3045982188@qq.com>
2021-02-16 00:46:23 +0800

    Update Hexo Static Content

---
+++ index.js
@@ -0,0 +1,42 @@
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="\assets\css\APlayer.min.css"><script src="\assets\js\APlayer.min.js" class="aplayer-secondary-script-marker"></script>const Waline = require('@waline/vercel');
+
+module.exports = Waline({
+  secureDomains: 'waline.js.org'
+
+  forbiddenWords: [
+    '傻逼',
+    '智障',
+    '没妈',
+    '草泥马',
+    '操你妈'
+  ]
+
+  async preSave(comment) {
+    const isSapm = await Akismet.check(comment);
+    if(isSpam) {
+      return { errmsg: 'It\'s a spam!' };
+    }
+  }
+
+  async postSave(comment, pComment) {
+    await mailto({
+      mail: pComment.mail,
+      text: `${comment.nick} replied your comment!`
+    });
+  }
+
+  async preUpdate(comment) {
+    return 'Then you can\'t update comment data';
+
+  async postUpdate(comment) {
+    console.log(`comment ${comment.objectId} has been updated!`);
+  }
+
+  async preDelete(commentId) {
+    return 'Then you can\'t delete comment';
+  }
+
+  async postDelete(commentId) {
+    console.log(`comment ${commentId} has been deleted!`);
+  }
+});
